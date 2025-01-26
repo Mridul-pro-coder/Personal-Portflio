6dfb4eaef711435828d3963acf4e6fdabc09439f
Mridul-pro-coder <mridulguptayt@gmail.com>
2025-01-26 15:30:20 +0530

    Deployed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,9 +23,9 @@ const Contact = () => {
                 </li>
             </ul>
             <form action="https://getform.io/f/awnnjwxb" method="POST" className="max-w-6xl p-5 md:p-12" id="form">
-                <input type="text" id="name" placeholder="Your Name" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4  " />
-                <input type="email" id="email" placeholder="Your Email" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4  "  />
-                <textarea type="textarea" id="textarea" cols="30" rows="4" placeholder="Your Message..." className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4  " />
+                <input type="text" id="name" name="fullname" placeholder="Your Name" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4  " />
+                <input type="email" id="email" name="youremail" placeholder="Your Email" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4  "  />
+                <textarea type="textarea" id="message" name="yourmessage" cols="30" rows="4" placeholder="Your Message..." className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4  " />
                 <button type="submit" className="w-full py-3 rounded-md text-gray-200 font-semibold text-xl
                 bg-gradient-to-r from-[#e78636] via-[#d17402]  to-[#e84900]">Send Message</button>
             </form>
